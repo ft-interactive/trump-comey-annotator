50d43e8fbf00d2f7b30972ff9680cc0fd340d417
David Blood <davidbjourno@users.noreply.github.com>
2017-05-10 11:01:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# trump-comey-annotator
+
+The [European Commission’s opening Brexit bid decoded](https://ig.ft.com/non-paper-annotated), April 20, 2017. 
+
+Based on previous versions at [trump-first-address](https://github.com/ft-interactive/trump-first-address), [federal-reserve-march-meeting-2017](https://github.com/ft-interactive/federal-reserve-march-meeting-2017) and [article-50-annotations](https://github.com/ft-interactive/article-50-annotations)
+
+[![Build Status][circle-image]][circle-url] [![Dependency Status][devdeps-image]][devdeps-url]
+
+## Local
+
+```
+npm start
+```
+
+Build/compile, start a dev server and watches for changes.
+
+# Deploy
+
+1. Write code in a branch.
+2. Make a PR. CI will automatically:
+    * build and test the branch
+    * deploy green builds to the review site
+3. Do quick smoke testing of the review build
+4. Get a code review. Once you get a thumbs up, merge into master.
+5. CI will build, test and deploy a build to Production.
+
+
+## Uses Starter Kit
+
+This project was scaffolded with [Starter Kit @5c74128](https://github.com/ft-interactive/starter-kit/tree/5c74128).
+
+## Licence
+This software is published by the Financial Times under the [MIT licence](http://opensource.org/licenses/MIT).
+
+Please note the MIT licence includes only the software, and does not cover any FT content made available using the software, which is copyright &copy; The Financial Times Limited, all rights reserved. For more information about re-publishing FT content, please contact our [syndication department](http://syndication.ft.com/).
+
+<!-- badge URLs -->
+[circle-url]: https://circleci.com/gh/ft-interactive/non-paper-annotated 
+[circle-image]: https://circleci.com/gh/ft-interactive/non-paper-annotated /tree/master.svg?style=shield
+
+[devdeps-url]: https://david-dm.org/ft-interactive/non-paper-annotated #info=devDependencies
+[devdeps-image]: https://img.shields.io/david/dev/non-paper-annotated /trump-first-address.svg?style=flat-square
---
+++ config/article.js
@@ -0,0 +1,102 @@
+export default () => ({ // eslint-disable-line
+
+  // link file UUID
+  id: 'b63ee296-3564-11e7-bce4-9023f8c0fd2e',
+
+  // canonical URL of the published page
+  // https://ig.ft.com/sites/federal-reserve-march-meeting-2017 get filled in by the ./configure script
+  url: 'https://ig.ft.com/non-paper-annotated/',
+
+  // To set an exact publish date do this:
+  //       new Date('2016-05-17T17:11:22Z')
+  publishedDate: new Date(),
+
+  headline: 'Brussels’ opening Brexit bid - annotated',
+
+  // summary === standfirst (Summary is what the content API calls it)
+  summary: 'How to read between the lines of the European Commission’s negotiating paper',
+
+  topic: {
+    name: 'Brexit',
+    url: 'https://www.ft.com/topics/themes/Brexit',
+  },
+
+  relatedArticle: {
+    text: '',
+    url: '',
+  },
+
+  mainImage: {
+    title: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ae38e8ce4-25ef-11e7-a34a-538b4cb30025?source=ig&width=2048&height=1152',
+    description: '',
+    url: '',
+    width: 2048, // ensure correct width
+    height: 1152, // ensure correct height
+  },
+
+  // Byline can by a plain string, markdown, or array of authors
+  // if array of authors, url is optional
+  byline: [
+    { name: 'Alex Barker', url: 'https://www.ft.com/stream/authorsId/Q0ItMDAwMDY3Mg==-QXV0aG9ycw==' },
+    { name: 'David Blood', url: 'https://www.ft.com/david-blood' },
+  ],
+
+  // Appears in the HTML <title>
+  title: 'The EU Brexit guidelines decoded',
+
+  // meta data
+  description: 'The full text of the European Council\'s guidance for handling' +
+    'Britain\'s withdrawal',
+
+  /*
+  TODO: Select Twitter card type -
+        summary or summary_large_image
+
+        Twitter card docs:
+        https://dev.twitter.com/cards/markup
+  */
+  twitterCard: 'summary',
+
+  /*
+  TODO: Do you want to tweak any of the
+        optional social meta data?
+  */
+  // General social
+  socialImage: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ae38e8ce4-25ef-11e7-a34a-538b4cb30025?source=ig&width=1200&fit=scale-down',
+  socialHeadline: 'The full text of the EU guidance on the Brexit negotiations',
+  // socialSummary: 'The UK's Article 50 notification letter, annotated',
+
+  // TWITTER
+  twitterImage: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ae38e8ce4-25ef-11e7-a34a-538b4cb30025?source=ig&width=1200&fit=scale-down',
+  twitterCreator: '@ft',
+  tweetText: 'The full text of the EU guidance on the Brexit negotiations, ' +
+    'annotated by FT journalists #brexit',
+  twitterHeadline: 'The full text of the EU guidance on the Brexit negotiations',
+
+  // FACEBOOK
+  facebookImage: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3Ae38e8ce4-25ef-11e7-a34a-538b4cb30025?source=ig&width=1200&fit=scale-down',
+  facebookHeadline: 'The full text of the EU guidance on the Brexit negotiations',
+
+  tracking: {
+
+    /*
+
+    Microsite Name
+
+    e.g. guffipedia, business-books, baseline.
+    Used to query groups of pages, not intended for use with
+    one off interactive pages. If you're building a microsite
+    consider more custom tracking to allow better analysis.
+    Also used for pages that do not have a UUID for whatever reason
+    */
+    // micrositeName: '',
+
+    /*
+    Product name
+
+    This will usually default to IG
+    however another value may be needed
+    */
+    // product: '',
+  },
+});
